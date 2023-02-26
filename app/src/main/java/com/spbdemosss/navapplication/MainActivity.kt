5d948febd82ec4be5cdd357cf35efa37d86cd162
demosss <spbdemosss@gmail.com>
2023-02-26 14:55:32 +0300

    1

--- app/src/main/java/com/spbdemosss/navapplication/MainActivity.kt
+++ app/src/main/java/com/spbdemosss/navapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.spbdemosss.navapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.spbdemosss.navapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        onBottomNavClick()
+    }
+
+    private fun onBottomNavClick(){
+        binding.bNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.menu_map -> Toast.makeText(this, "Map", Toast.LENGTH_SHORT).show()
+                R.id.menu_points -> Toast.makeText(this, "Points", Toast.LENGTH_SHORT).show()
+                R.id.menu_navigation -> Toast.makeText(this, "Navigation", Toast.LENGTH_SHORT).show()
+                R.id.menu_orientation -> Toast.makeText(this, "Orientation", Toast.LENGTH_SHORT).show()
+                R.id.menu_settings -> Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
     }
 }
